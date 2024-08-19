c10f08055ec8f0d9b5a70b9c477f0a200db23302
wizenheimer <nayan.kumar@couchbase.com>
2024-08-19 19:01:53 +0530

    feat(model): colocate types within columns

--- package/python/cyyrus/models/column.py
+++ package/python/cyyrus/models/column.py
@@ -1,10 +1,21 @@
-from typing import List
+from typing import List, Union
 
 from pydantic import BaseModel, Field
 
+from cyyrus.models.types import (
+    StaticArrayModel,
+    StaticBooleanModel,
+    StaticFloatModel,
+    StaticIntegerModel,
+    StaticStringModel,
+)
+
 
 class Column(BaseModel):
-    column_type: str = Field(..., description="Type of the column")
+    column_type: str = Field(
+        ...,
+        description="Type of the column",
+    )
     description: str = Field(
         default="",
         description="Description of the column",
@@ -16,4 +27,16 @@ class Column(BaseModel):
     task_input: List[str] = Field(
         default=[],
         description="Input to the task",
+    )
+    pydantic_model: Union[
+        type[BaseModel],
+        StaticStringModel,
+        StaticBooleanModel,
+        StaticFloatModel,
+        StaticIntegerModel,
+        StaticArrayModel,
+    ] = Field(
+        default=StaticStringModel,
+        exclude=True,
+        description="Dynamic model created based on data inside CustomType and TaskType",
     )
