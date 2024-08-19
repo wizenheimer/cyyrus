12371306df834000318bde23c978197c49643b21
wizenheimer <nayan.kumar@couchbase.com>
2024-08-19 19:06:02 +0530

    feat(types): add default catch all task

--- package/python/cyyrus/tasks/default.py
+++ package/python/cyyrus/tasks/default.py
@@ -1,13 +1,48 @@
 from typing import Any, Dict
 
-from cyyrus.tasks.base import BaseTask
+from pydantic.main import BaseModel
+
+from cyyrus.models.task import TaskType
+from cyyrus.models.types import (
+    StaticArrayModel,
+    StaticBooleanModel,
+    StaticFloatModel,
+    StaticIntegerModel,
+    StaticStringModel,
+    get_static_model,
+)
 
 
-class DefaultTask(BaseTask):
-    TASK_ID = "default_task"
+class DefaultTask:
+    TASK_ID = TaskType.DEFAULT
+
+    def __init__(
+        self,
+        task_properties: Dict[str, Any],
+        task_model: (
+            BaseModel
+            | StaticIntegerModel
+            | StaticFloatModel
+            | StaticBooleanModel
+            | StaticStringModel
+            | StaticArrayModel
+        ),
+    ) -> None:
+        self.task_properties = task_properties
+        self.task_model = get_static_model(TaskType.DEFAULT).model_construct(values="Default Task")
+
+    def execute(
+        self,
+        task_input: Dict[str, Any],
+    ):
+        return self.task_model.model_construct(
+            value=self._execute(
+                task_input,
+            )
+        )
 
     def _execute(
         self,
         task_input: Dict[str, Any],
-    ):
-        return self.task_model()  # type: ignore
+    ) -> Any:
+        return f"Default Task: {task_input} with Task Properties: {self.task_properties}"
