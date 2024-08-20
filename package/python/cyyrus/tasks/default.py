91d119f960911ae11d9ec5758a42982b9fb32047
wizenheimer <nayan.kumar@couchbase.com>
2024-08-21 00:44:28 +0530

    feat(task): add support for default tasks

--- package/python/cyyrus/tasks/default.py
+++ package/python/cyyrus/tasks/default.py
@@ -1,45 +1,11 @@
 from typing import Any, Dict
 
-from pydantic.main import BaseModel
-
-from cyyrus.models.task import TaskType
-from cyyrus.models.types import (
-    StaticArrayModel,
-    StaticBooleanModel,
-    StaticFloatModel,
-    StaticIntegerModel,
-    StaticStringModel,
-    get_static_model,
-)
+from cyyrus.models.task_type import TaskType
+from cyyrus.tasks.base import BaseTask
 
 
-class DefaultTask:
+class DefaultTask(BaseTask):
     TASK_ID = TaskType.DEFAULT
-
-    def __init__(
-        self,
-        task_properties: Dict[str, Any],
-        task_model: (
-            BaseModel
-            | StaticIntegerModel
-            | StaticFloatModel
-            | StaticBooleanModel
-            | StaticStringModel
-            | StaticArrayModel
-        ),
-    ) -> None:
-        self.task_properties = task_properties
-        self.task_model = get_static_model(TaskType.DEFAULT).model_construct(values="Default Task")
-
-    def execute(
-        self,
-        task_input: Dict[str, Any],
-    ):
-        return self.task_model.model_construct(
-            value=self._execute(
-                task_input,
-            )
-        )
 
     def _execute(
         self,
