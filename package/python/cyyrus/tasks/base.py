9e821161e0691ea907e2fcb061ecd997c681f10c
wizenheimer <nayan.kumar@couchbase.com>
2024-08-22 01:00:55 +0530

    feat(task): make task execution type more explicit

--- package/python/cyyrus/tasks/base.py
+++ package/python/cyyrus/tasks/base.py
@@ -1,4 +1,4 @@
-import warnings
+import difflib
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List
 
@@ -6,7 +6,11 @@ from cyyrus.models.task_type import TaskType
 
 
 class BaseTask(ABC):
+    # The task ID is used to identify the task type
     TASK_ID = TaskType.DEFAULT
+
+    # This flag indicates whether the task supports reference-free execution
+    SUPPORTS_REFERENCE_FREE_EXECUTION = True
 
     def __init__(
         self,
@@ -16,47 +20,158 @@ class BaseTask(ABC):
         self.task_properties = task_properties
         self.column_name = column_name
 
+    def reference_based_execution(
+        self,
+        task_input: Dict[str, Any],
+    ) -> Dict[str, Any]:
+        """
+        Perform a reference-based execution
+
+        Strategy: Incase the task is reference-free, the task_input will be None and the task will attempt to generate the reference data. The final output will be a list of dictionaries.
+        """
+        task_result = {
+            self.column_name: (
+                self.execute(task_input)
+                if not self._trigger_flattening()
+                else self._flatten_dict(self.execute(task_input))
+            )
+        }
+        return task_result
+
+    def reference_free_execution(
+        self,
+    ) -> List[Dict[str, Any]]:
+        """
+        Perform a reference-free execution.
+
+        Strategy: Incase the task is reference-based, the task_input will be a dictionary containing the reference data. The final output will be a dictionary.
+        """
+        if not self.SUPPORTS_REFERENCE_FREE_EXECUTION:
+            return []
+
+        task_inputs = self._generate_references()
+        task_results = []
+        for task_input in task_inputs:
+            task_results.append(self.reference_based_execution(task_input))
+        return task_results
+
+    # The execute method is the main method that needs to be implemented by the task
+    @abstractmethod
+    def execute(
+        self,
+        task_input: Dict[str, Any],
+    ) -> Any:
+        """
+        Perform the task execution
+        """
+        return None
+
+    # Incase the task supports reference-free execution, the task should implement the _generate_references method as well
+    def _generate_references(
+        self,
+    ) -> List[Dict[str, Any]]:
+        """
+        Attempt to generate the reference data for the task, using the task properties
+        """
+        return []
+
+    # =====================================
+    #           Utility methods
+    # =====================================
     @staticmethod
-    def flatten_dict(
+    def _flatten_dict(
         d: Dict[str, Any],
         parent_key: str = "",
         sep: str = "_",
         max_depth: int = 5,
     ) -> Dict[str, Any]:
+        """
+        Attempts to flatten the dictionary to a single level, by concatenating the keys with the separator. Upto the specified max_depth.
+        """
         items = []
         for k, v in d.items():
             new_key = f"{parent_key}{sep}{k}" if parent_key else k
             if isinstance(v, dict) and max_depth > 1:
-                items.extend(BaseTask.flatten_dict(v, new_key, sep, max_depth - 1).items())
+                items.extend(BaseTask._flatten_dict(v, new_key, sep, max_depth - 1).items())
             else:
                 items.append((new_key, v))
         return dict(items)
 
-    def execute(
+    def _get_task_property(
         self,
-        task_input: Dict[str, Any],
-    ) -> List[Dict[str, Any]]:
-        # Execute the task, and get the result
-        try:
-            result = self._execute(task_input)
-        except Exception as e:
-            warnings.warn(f"Error executing task {self.column_name}: {str(e)}")
-            result = "Err"
-
-        # If the result is a dictionary and the flatten property is set to True, flatten the dictionary
-        if isinstance(result, dict) and self.task_properties.get("flatten", False):
-            result = self.flatten_dict(result)
-
-        # The final result should be a list of dictionaries, to be consistent with the output of all tasks and make it simpler to merge them into a dataframe
-        return [
-            {
-                self.column_name: result,
-            }
-        ]
-
-    @abstractmethod
-    def _execute(
-        self,
-        task_input: Dict[str, Any],
+        key: str,
+        default: Any = None,
     ) -> Any:
-        return f"Default Task: {task_input} with Task Properties: {self.task_properties}"
+        """
+        The key used to extract the path from the task properties, incase the key is not found, the task will try to find the closest matching key
+        """
+        if key in list(self.task_properties.keys()):
+            property_value = self.task_properties.get(
+                key,
+                default,
+            )
+        else:
+            closest_key = self._find_closest_key(
+                list(self.task_properties.keys()),
+                key,
+            )
+            property_value = self.task_properties.get(
+                closest_key,
+                default,
+            )
+        return property_value
+
+    def _get_task_input(
+        self,
+        key: str,
+        task_input: Dict[str, Any],
+        default: Any = None,
+    ) -> Any:
+        """
+        The key used to extract the path from the task input, incase the key is not found, the task will try to find the closest matching key
+        """
+        if key in list(task_input.keys()):
+            property_value = task_input.get(
+                key,
+                default,
+            )
+        else:
+            closest_key = self._find_closest_key(
+                list(task_input.keys()),
+                key,
+            )
+            property_value = task_input.get(
+                closest_key,
+                default,
+            )
+        return property_value
+
+    def _find_closest_key(
+        self,
+        keys: List[str],
+        target: str,
+    ) -> str:
+        """
+        Find the closest matching key from the list of keys.
+        """
+        if not keys:
+            return ""
+        return max(
+            keys,
+            key=lambda k: difflib.SequenceMatcher(
+                None,
+                k,
+                target,
+            ).ratio(),
+        )
+
+    def _trigger_flattening(
+        self,
+    ) -> bool:
+        """
+        Check if the task should trigger flattening
+        """
+        return self.task_properties.get(
+            "flatten",
+            False,
+        )
