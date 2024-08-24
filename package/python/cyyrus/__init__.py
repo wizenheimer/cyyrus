689f8cb67b8491b7355f254b9d812069ad49dd30
wizenheimer <nayan.kumar@couchbase.com>
2024-08-24 06:23:08 +0530

    feat(core): expose external interface for package

--- package/python/cyyrus/__init__.py
+++ package/python/cyyrus/__init__.py
@@ -1,11 +1,17 @@
-import logging
+# import logging
 
-from .utils.logging import get_logger, setup_logging
+from .models.spec import load_spec
+
+# from .utils.logging import get_logger, setup_logging
 
 # Set up logging for the entire package
-setup_logging(
-    log_level=logging.INFO,
-    log_file="cyyrus.log",
-    for_human=True,
-)
-logger = get_logger()
+# setup_logging(
+#     log_level=logging.DEBUG,
+#     log_file="cyyrus.log",
+#     for_human=True,
+# )
+# logger = get_logger()
+
+__all__ = [
+    "load_spec",
+]
